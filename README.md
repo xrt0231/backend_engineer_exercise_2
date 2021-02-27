7db0b2b066580aff3a6fa9f73dd748d353885734
xrt0231 <jtsw@protonmail.com>
2021-02-27 23:35:43 +0800

    revise Q4.1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Backend engineer exercise part 2 (Q4)
 
-1. Please review top.js in src directory as answer of Q4.1.  
+1. Please review index.js in src directory as answer of Q4.1.  
+   Use command in CLI as $alex <api-key> <number> to get top <number> sites URL on www.alexa.com/topsites/
--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 #!/usr/bin/env node
 
-require = require('esm')(module);
-require('./src/cli').cli(process.argv);
+const rp   = require('request-promise')
+const util = require('util');
+const readline = require('readline');
 
+const apiHost = 'ats.api.alexa.com'
+
+global.fetch = require("node-fetch");
+
+console.log(process.argv);
+console.log(process.argv[2], process.argv[3]);
+console.log(process.argv.length);
+
+function callATS(apikey, num) {
+  var uri = '/api?Action=TopSites&Count=' + num + '&CountryCode=US&ResponseGroup=Country&Output=json';
+
+  var opts = {
+    host: apiHost,
+    path: uri,
+    uri: 'https://' + apiHost + uri,
+    json: true,
+    headers: {'x-api-key': apikey},
+        resolveWithFullResponse: true
+  }
+
+  rp(opts)
+  .then(function (html){ 
+  for(let i=0; i<num; i++){
+    
+    console.log(`${JSON.stringify(html.body.Ats.Results.Result.Alexa.TopSites.Country.Sites.Site[i].DataUrl, null, 2)}`)
+    }
+  })
+
+  .catch( (e)=> console.log('failed:'+e))
+}
+
+if (process.argv.length != 4) {
+  console.log(`Usage: node ${process.argv[1]} APIKEY COUNTRY`);
+  process.exit(0);
+}
+
+callATS(process.argv[2], process.argv[3]);
 
 
